bab44f97d902f649a4275b7034307dbf96c5d5fd
Alexandre <alexandre.bessa@academico.ifg.edu.br>
2025-08-26 14:45:29 -0300

    Resolucao do exercicio

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,1 +1,12 @@
 # Sua solução aqui
+entrada = input().split(" ")
+altura = float(entrada[0])
+genero = entrada[1]
+
+if "M" in genero:
+    peso = (72.7 * altura) - 58
+
+if "F" in genero:
+    peso = (62.1 * altura) - 44.7
+
+print(f"{peso:.2f}")
